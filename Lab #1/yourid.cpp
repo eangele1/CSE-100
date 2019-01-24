b461040c2573104bc87eeed7966540b79cdaa5a6
eangele1 <ezerangeles1@gmail.com>
2019-01-24 12:48:03 -0800

    Update yourid.cpp

--- Lab #1/yourid.cpp
+++ Lab #1/yourid.cpp
@@ -2,44 +2,66 @@
 
 using namespace std;
 
+//funciton to executre insertion sort algorithm. Takes in array and array size as parameters.
 void InsertionSort(int arr[], int arrSize){
-    
-    int key;
-    int counter = 1;
-    
-    for (int i = 1; i < arrSize; i++){
-        key = arr[i];
-        int j = i - 1;
-        
-        while(j >= 0 && arr[j] > key){
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-        
-        counter++;
-        
-        for (int i = 0; i < counter; i++){
-            cout << arr[i] << ";";
-        }
-        cout << endl;
-    }
-    
+
+	//initialize an int variable to store current key to compare with array elements
+	int key;
+
+	//initialize an int variable to keep track of how many elements to output at once to console
+	int counter = 1;
+
+	//iterates through array and sorts elements; two at a time.
+	for (int i = 1; i < arrSize; i++){
+
+		//key is equal to element at ith position in array
+		key = arr[i];
+
+		//initialize an int variable to store ith position minus one
+		int j = i - 1;
+
+		//while jth position is greater than or equal to zero, and element at j is greater than the key,
+		//element at j plus one is now element at j. J is one position less.
+		while(j >= 0 && arr[j] > key){
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		//element at j plus one is now the key
+		arr[j + 1] = key;
+
+		//increment counter by one
+		counter++;
+
+		//loops through array and prints out counter number of elements in a line.
+		for (int i = 0; i < counter; i++){
+			cout << arr[i] << ";";
+		}
+
+		//adds an enter line after printing out line of elements.
+		cout << endl;
+	}
+
 }
 
 int main(int argc, char const *argv[]){
-    
-    int arrSize;
-    
-    cin >> arrSize;
-    
-    int arr[arrSize];
-    
-    for (int i = 0; i < arrSize; i++){
-        cin >> arr[i];
-    }
-    
-    InsertionSort(arr, arrSize);
-    
-    return 0;
+
+    //initialize variable to store array size
+	int arrSize;
+
+    //takes in integer for array size
+	cin >> arrSize;
+
+    //initialize array to store integer elements
+	int arr[arrSize];
+
+    //loops through slots in array and inserts input value
+	for (int i = 0; i < arrSize; i++){
+		cin >> arr[i];
+	}
+
+    //sorts the array and outputs snapshot of each iteration
+	InsertionSort(arr, arrSize);
+
+	return 0;
 }
